41ca0bce1ca2086d429d3691e9d7d4929dbbc34b
Ynneh <007trey2k@gmail.com>
2023-03-08 16:36:07 +0000

    dummy class

--- src/test/java/io/ynneh/NPCSpawnDumperTest.java
+++ src/test/java/io/ynneh/NPCSpawnDumperTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package io.ynneh;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class NPCSpawnDumperTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(NPCSpawnDumper.class);
 		RuneLite.main(args);
 	}
 }
