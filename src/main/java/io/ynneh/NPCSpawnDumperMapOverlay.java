5dbe70d6c5575b53855513a9d349d01b4c449fd6
Ynneh <007trey2k@gmail.com>
2023-03-17 16:04:56 +0000

    Added region lines to world / minimap

--- src/main/java/io/ynneh/NPCSpawnDumperMapOverlay.java
+++ src/main/java/io/ynneh/NPCSpawnDumperMapOverlay.java
@@ -1,21 +1,20 @@
 package io.ynneh;
 
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics2D;
-import javax.inject.Inject;
-import lombok.AccessLevel;
-import lombok.Setter;
-import net.runelite.api.GraphicID;
+import com.google.inject.Inject;
+import net.runelite.api.Client;
 import net.runelite.api.NPC;
-import net.runelite.api.NpcID;
 import net.runelite.api.Point;
-import net.runelite.client.eventbus.Subscribe;
-import net.runelite.client.game.FishingSpot;
+import net.runelite.api.RenderOverview;
+import net.runelite.api.widgets.Widget;
+import net.runelite.api.widgets.WidgetInfo;
 import net.runelite.client.ui.overlay.Overlay;
 import net.runelite.client.ui.overlay.OverlayLayer;
 import net.runelite.client.ui.overlay.OverlayPosition;
 import net.runelite.client.ui.overlay.OverlayUtil;
+
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
+
 /**
  * @author Ynneh | 17/03/2023 - 12:25
  * <https://github.com/drhenny>
@@ -25,8 +24,15 @@ public class NPCSpawnDumperMapOverlay extends Overlay {
     private final NPCSpawnDumper plugin;
     private final NPCSpawnDumperConfig config;
 
+    private static final int LABEL_PADDING = 4;
+    private static final Color WHITE_TRANSLUCENT = new Color(255, 255, 255, 127);
+
     @Inject
-    public NPCSpawnDumperMapOverlay(NPCSpawnDumper plugin, NPCSpawnDumperConfig config) {
+    private final Client client;
+
+    @Inject
+    public NPCSpawnDumperMapOverlay(Client client, NPCSpawnDumper plugin, NPCSpawnDumperConfig config) {
+        this.client = client;
         setPosition(OverlayPosition.DYNAMIC);
         setLayer(OverlayLayer.ABOVE_WIDGETS);
         this.plugin = plugin;
@@ -36,6 +42,7 @@ public class NPCSpawnDumperMapOverlay extends Overlay {
     @Override
     public Dimension render(Graphics2D graphics) {
 
+        drawMapLines(graphics, 64, Color.GREEN);
 
             plugin.getSpawns().keySet().forEach(v -> {
 
@@ -54,5 +61,70 @@ public class NPCSpawnDumperMapOverlay extends Overlay {
             });
 
         return null;
+    }
+
+    private void drawMapLines(Graphics2D graphics, int gridSize, Color gridColour) {
+        final int gridTruncate = ~(gridSize - 1);
+
+        RenderOverview ro = client.getRenderOverview();
+        Widget map = client.getWidget(WidgetInfo.WORLD_MAP_VIEW);
+        Float pixelsPerTile = ro.getWorldMapZoom();
+
+        if (map == null) {
+            return;
+        }
+
+        if (gridSize * pixelsPerTile < 3) {
+            return;
+        }
+
+        Rectangle worldMapRect = map.getBounds();
+        graphics.setClip(worldMapRect);
+
+        int widthInTiles = (int) Math.ceil(worldMapRect.getWidth() / pixelsPerTile);
+        int heightInTiles = (int) Math.ceil(worldMapRect.getHeight() / pixelsPerTile);
+
+        Point worldMapPosition = ro.getWorldMapPosition();
+
+        // Offset in tiles from anchor sides
+        int yTileMin = worldMapPosition.getY() - heightInTiles / 2;
+        int xRegionMin = (worldMapPosition.getX() - widthInTiles / 2) & gridTruncate;
+        int xRegionMax = ((worldMapPosition.getX() + widthInTiles / 2) & gridTruncate) + gridSize;
+        int yRegionMin = (yTileMin & gridTruncate);
+        int yRegionMax = ((worldMapPosition.getY() + heightInTiles / 2) & gridTruncate) + gridSize;
+        int regionPixelSize = (int) Math.ceil(gridSize * pixelsPerTile);
+
+        for (int x = xRegionMin; x < xRegionMax; x += gridSize) {
+            for (int y = yRegionMin; y < yRegionMax; y += gridSize) {
+                int yTileOffset = -(yTileMin - y);
+                int xTileOffset = x + widthInTiles / 2 - worldMapPosition.getX();
+
+                int xPos = ((int) (xTileOffset * pixelsPerTile)) + (int) worldMapRect.getX();
+                int yPos = (worldMapRect.height - (int) (yTileOffset * pixelsPerTile)) + (int) worldMapRect.getY();
+                // Offset y-position by a single region to correct for drawRect starting from the top
+                yPos -= regionPixelSize;
+
+                graphics.setColor(gridColour);
+
+                graphics.drawRect(xPos, yPos, regionPixelSize, regionPixelSize);
+
+                graphics.setColor(WHITE_TRANSLUCENT);
+
+                if (gridSize == 64) {
+                    int regionId = ((x >> 6) << 8) | (y >> 6);
+                    String regionText = String.valueOf(regionId);
+                    FontMetrics fm = graphics.getFontMetrics();
+                    Rectangle2D textBounds = fm.getStringBounds(regionText, graphics);
+                    int labelWidth = (int) textBounds.getWidth() + 2 * LABEL_PADDING;
+                    int labelHeight = (int) textBounds.getHeight() + 2 * LABEL_PADDING;
+                    graphics.fillRect(xPos, yPos, labelWidth, labelHeight);
+                    graphics.setColor(Color.BLACK);
+                    graphics.drawString(
+                            regionText,
+                            xPos + LABEL_PADDING,
+                            yPos + (int) textBounds.getHeight() + LABEL_PADDING);
+                }
+            }
+        }
     }
 }
